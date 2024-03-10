46c3efad2f971e21e38b7a72a46c425bd7d229a5
Ahsanul HP <hqahsanul@gmail.com>
2024-03-11 01:14:12 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,36 @@
+function findSpareChairs(Rooms, need) {
+    if (need === 0) {
+        return [1];
+    }
+
+    const chairsTaken = [];
+    let t_ChairsNeeded = need;
+
+    for (const room of Rooms) {
+        const [occupants, totalChairs] = room;
+        const occupiedChairs = occupants.length;
+        const spareChairs = Math.max(0, totalChairs - occupiedChairs);
+
+        if (spareChairs >= t_ChairsNeeded) {
+            chairsTaken.push(t_ChairsNeeded);
+            return chairsTaken;
+        } else if (spareChairs > 0) {
+            chairsTaken.push(spareChairs);
+            t_ChairsNeeded -= spareChairs;
+        } else if (t_ChairsNeeded>0&&spareChairs==0){
+            chairsTaken.push(spareChairs);
+        }
+
+    }
+
+    if (t_ChairsNeeded > 0) {
+        return [0];
+    }
+
+    return chairsTaken;
+}
+
+const meetingRooms = [['XXX', 3], ['XXXXX', 6], ['XXXXXX', 9], ['XXXXXXX', 7],['XXXXXXX', 8]];
+const needChairs = 5;
+const result = findSpareChairs(meetingRooms, needChairs);
+console.log(result);
